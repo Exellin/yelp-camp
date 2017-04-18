e71ccd0ee973d2554883b2f1ebc4044802856e7f
exellin <carnegieshawn@yahoo.ca>
2017-04-18 00:50:17 +0000

    add campgrounds page

--- app.js
+++ app.js
@@ -3,10 +3,19 @@ var app = express();
 
 app.set("view engine", "ejs");
 
-app.get("/", function(request, response) {
+app.get("/", function(request, response){
   response.render("landing");
 });
 
-app.listen(process.env.PORT, process.env.IP, function() {
+app.get("/campgrounds", function(request, response){
+  var campgrounds = [
+    {name: "Salmon Creek", image: "https://farm9.staticflickr.com/8442/7962474612_bf2baf67c0.jpg"},
+    {name: "Granite Hill", image: "https://farm1.staticflickr.com/60/215827008_6489cd30c3.jpg"},
+    {name: "Mountain Goat's Rest", image: "https://farm7.staticflickr.com/6057/6234565071_4d20668bbd.jpg"}
+  ];
+  response.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
   console.log("Server started");
 })
