085fee389579aa06e36ddd78fde0b35ed9d3d294
exellin <carnegieshawn@yahoo.ca>
2017-06-12 17:04:57 +0000

    add configuration methods for passport

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var express       = require("express"),
     bodyParser    = require('body-parser'),
     mongoose      = require("mongoose"),
     passport      = require("passport"),
-    localStrategy = require("passport-local"),
+    LocalStrategy = require("passport-local"),
     Campground    = require("./models/campground"),
     Comment       = require("./models/comment"),
     User          = require("./models/user"),
@@ -14,6 +14,18 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
 seedDB();
+
+app.use(require("express-session")({
+  secret: "this is a secret key",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 app.get("/", function(request, response){
   response.render("landing");
